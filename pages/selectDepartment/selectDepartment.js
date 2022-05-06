3e7e8fe2b525a763ae6c5515e0cd92cfabd26f2f
Graham <3235962608@qq.com>
2022-05-06 23:01:30 +0800

    ...

--- pages/selectDepartment/selectDepartment.js
+++ pages/selectDepartment/selectDepartment.js
@@ -5,6 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    activeKey: 0,
+    About: [],
+    users: [
+      {
+          "id": 1,
+          "name": "全部"
+      },
+      {
+          "id": 2,
+          "name": "中考真题"
+      },
+      {
+          "id": 3,
+          "name": "化学"
+      },
+      {
+          "id": 4,
+          "name": "英语"
+      }
+  ]
 
   },
 
@@ -12,55 +32,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    // var baseJS = require("../../common/tools/base");
+    // baseJS.getData("hospital_test", this.getRes);
+    // var list = [2, "hello"];
+    // baseJS.addData("hospital_test", list);
+    // baseJS.getData("goods", this.getRes)
+    console.log(this.data.users)
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
 
-  },
+ // 回调函数
+ getRes(res) {
+  console.log("你好", res);
+  this.setData({
+    About: res.data
+  })
+  console.log(this.data.About)
+}
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
